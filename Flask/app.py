00e3811ead5e35ea548a0436a5c2ca838c376710
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-05-27 20:13:06 -0600

    rutas con flask

--- Flask/app.py
+++ Flask/app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+#RUTA SIMPLE
+@app.route('/')
+def principal():
+    return 'Hola Mundo Flask'
+
+#RUTA DOBLE 
+@app.route('/usuario')
+@app.route('/saludar')
+def saludos():
+    return 'Hola Michelle Quintero'
+
+#RUTA CON PARAMENTROS 
+@app.route('/hi/<nombre>')
+def hi(nombre):
+    return 'Hola' + nombre + '!!!'
+
+#DEFINICION DE LOS METODOS DE TRABAJO 
+@app.route('/formulario', methods = ['GET','POST'])
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar password por GET'
+    elif request.method == 'POST':
+        return 'POST si es seguro para password'
+
+#Manejo de excepciones para rutas 
+@app.errorhandler(404)
+def paginano(e):
+    return 'Revisa tu sintaxis: No encontre nada'
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
