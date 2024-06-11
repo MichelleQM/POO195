7939ed64f5dd21d58723227f3c061be72eec2a97
MichelleQM <148285778+MichelleQM@users.noreply.github.com>
2024-06-11 08:12:21 -0600

    Vistas Flask

--- CrudAlbums/app.py
+++ CrudAlbums/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, request,render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345'
+app.config['MYSQL_PASSWORD'] = ' '
 app.config['MYSQL_DB'] = 'dbflask'
+
+app.secret_key = 'millavesecreta'
 mysql = MySQL(app)
 
 @app.route('/')
@@ -15,16 +17,26 @@ def index():
 @app.route('/guardarAlbum', methods=['POST'])
 def guardarAlbum():
     if request.method == 'POST':
-        Titulo= request.form['txtTitulo']
-        Artista= request.form['txtArtista'] 
-        Año= request.form['txtAnio']
-        print(Titulo,Artista,Año)
-        return 'Datos recibidos en el Server'
+        # Tomamos los datos que vienen por POST
+        FTitulo = request.form['txtTitulo']
+        FArtista = request.form['txtArtista']
+        FAnio = request.form['txtAnio']
+        
+        # Enviamos a la BD
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO albums(titulo, artista, anio) VALUES (%s, %s, %s)', (FTitulo, FArtista, FAnio))
+        mysql.connection.commit()
+        
+        flash('Album Guardado correctamente')
+        return redirect(url_for('index'))
 
 @app.errorhandler(404)
 def paginanotfound(e):
-    return 'Revisa tu sintaxis: No encontre nada'
+    return 'Revisa tu sintaxis: No encontré nada'
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(port=3000, debug=True)
+
+
+
 
--- Flask/bdflask.sql
+++
@@ -1,1 +0,0 @@
-create database bdflask;
